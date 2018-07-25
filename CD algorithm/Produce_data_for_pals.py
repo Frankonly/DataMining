9f35c00dbb1df66c742cb74d040f01d41fc3ae6e
RioWu <283489710@qq.com>
2018-07-25 10:07:54 +0800

    Update Produce_data_for_pals.py

--- CD algorithm/Produce_data_for_pals.py
+++ CD algorithm/Produce_data_for_pals.py
@@ -1,42 +1,34 @@
 class Config():
     colors = []
-    labels = None
-
+    labels = ['0','1','2','3','4','5']
 # produce edges.dat
-def export_edge_list(sim, labels = Config.labels, filename, delim = ",", header = True):
+def export_edge_list(sim, filename, delim = ",", header = True, labels = Config.labels):
     f = open(filename, 'w')
     if header:
         f.write("Source,Target\n")
-    for i in range(sim.shape[0]):
-        for j in range(i+1, sim.shape[1]):
+    for i in range(len(sim[0])):
+        for j in range(i+1, len(sim[1])):
             if sim[i,j] != 0:
                 if labels == None:
                     f.write(str(i) + delim + str(j) + "\n")
                 else:
                     f.write("\"" + labels[i] + "\"" + delim + "\"" + labels[j] + "\"\n")                          
     f.close()
-    files.download(filename)
-
+    return
 #produce community.dat
-def export_community_list(communities, labels=Config.labels, filename):
-
+def export_community_list(communities, filename, labels=Config.labels):
     f = open("pals-community.dat", 'w')
-
     indices_in_community = []   
-
     cur_com = 1
-
     for c in communities:
         indices = [i for i, x in enumerate(Config.labels) if x in c]
+        print(indices)
         indices_in_community.extend(indices)
         for i in indices:
-            f.write("\"" + Config.labels[i] + "\" " + str(cur_com) + "\r\n")
+            f.write("\"" + Config.labels[i] + "\" " + str(cur_com) + "\n")
         cur_com += 1
-
     f.close()
-
-    files.download(filename)
-
-export_edge_list(adjmat,labels = Config.labels, filename = 'pals-edges.dat', delim = " ")
-
+    return
+adjmat = [['1'], ['2','5'], ['3','4','0']]
+#export_edge_list(adjmat,labels = Config.labels, filename = 'pals-edges.dat', delim = " ")
 export_community_list(adjmat,labels = Config.labels, filename = 'pals-communities.dat')  
