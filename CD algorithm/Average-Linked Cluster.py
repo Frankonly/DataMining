dfec79e606c47ed7a64241bceb8ff5fc79f2c48f
RioWu <283489710@qq.com>
2018-07-27 11:42:09 +0800

    对功能进行了封装

--- CD algorithm/Average-Linked Cluster.py
+++ CD algorithm/Average-Linked Cluster.py
@@ -1,3 +1,4 @@
+#data是不相似度矩阵
 data = [[0,662,877,255,412,966],[662,0,295,468,268,400],[877,295,0,754,564,138],[255,468,754,0,219,869],[412,268,564,219,0,669],[996,400,138,869,669,0]]
 copy = data.copy()
 length = len(data)
@@ -21,12 +22,15 @@ def find_min(M):
             if i !=j and M[i][j] < minvalue:
                 minvalue = M[i][j];x = i;y = j
     return (x,y,minvalue)
-for i in range(1,length):
-    x,y,minvalue = find_min(copy)
-    cluster[min(x,y)].extend(cluster[y])
-    cluster.remove(cluster[y])
-    copy = [[] for i in range(5)]
-    for i in range(len(cluster)):
-        for j in range(len(cluster)):
-            copy[i].extend([cal_ave(i,j,cluster)])
-    print(cluster)
+#将聚类功能进行了函数封装，k代表聚簇次数，alg代表聚类算法，cal_ave,cal_min,cal_max分别代表平均聚类，单链聚类和完全聚类,输出的是聚簇后的结果，用一个二维list表示
+def linked_cluster(k = length,alg = cal_ave,copy = copy):
+    for i in range(1,k):
+        x,y,minvalue = find_min(copy)
+        cluster[min(x,y)].extend(cluster[y])
+        cluster.remove(cluster[y])
+        copy = [[] for i in range(5)]
+        for i in range(len(cluster)):
+            for j in range(len(cluster)):
+                copy[i].extend([alg(i,j,cluster)])
+    return cluster
+print(linked_cluster())
