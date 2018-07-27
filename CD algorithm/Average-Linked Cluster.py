1257fc72da7c8b32693fd19a1e4dcfd026429742
RioWu <283489710@qq.com>
2018-07-27 19:49:38 +0800

    Update Average-Linked Cluster.py
    1.backup函数需要提前定义
    2.Config类需要提前定义（labels代表标签）
    3.cal_ave,cal_min,cal_max分别代表平均聚类，单链聚类和完全聚类

--- CD algorithm/Average-Linked Cluster.py
+++ CD algorithm/Average-Linked Cluster.py
@@ -1,6 +1,35 @@
 data = adjmat.copy()
 length = len(data)
 copy = data.copy()
+# produce edges.dat
+def export_edge_list(sim, filename, delim = ",", header = False, labels = Config.labels):
+    f = open(filename, 'w')
+    if header:
+        f.write("Source,Target\n")
+    for i in range(len(sim[0])):
+        for j in range(i+1, len(sim[1])):
+            if sim[i,j] != 0:
+                if labels == None:
+                    f.write(str(i) + delim + str(j) + "\n")
+                else:
+                    f.write("\"" + labels[i] + "\"" + delim + "\"" + labels[j] + "\"\n")                          
+    f.close()
+    backup(filename,False)
+    return
+#produce community.dat
+def export_community_list(communities, filename, labels=Config.labels):
+    f = open(filename, 'w')
+    indices_in_community = []   
+    cur_com = 1
+    for c in communities:
+        indices = [i for i, x in enumerate(Config.labels) if x in c]
+        indices_in_community.extend(indices)
+        for i in indices:
+            f.write("\"" + Config.labels[i] + "\" " + str(cur_com) + "\n")
+        cur_com += 1
+    f.close()
+    backup(filename,False)
+    return
 #计算各聚簇之间距离的平均值
 def cal_ave(i,j,cluster):
     sum = 0
@@ -48,4 +77,13 @@ def linked_cluster(alg = cal_ave,k = length,copy = copy):
               for j in range(len(cluster)):
                 copy[i].extend([alg(i,j,cluster)])
           print(cluster)
-linked_cluster(alg = cal_max,k = length)
+    return cluster
+cluster = linked_cluster(alg = cal_max,k = length - 8)
+#用电影名替换序号
+for i in range(len(cluster)):
+    for j in range(len(cluster[i])):
+        cluster[i][j] = Config.labels[cluster[i][j]]
+#生成作图所用的数据
+export_edge_list(sim,labels = Config.labels, filename = 'pals-edges.dat', delim = " ")
+export_community_list(cluster,labels = Config.labels, filename = 'pals-communities.dat')  
+
